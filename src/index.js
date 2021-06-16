afa44fe9fa19fe485e114a8705b7c9c47f8074a3
andrew <neon.duncan@gmail.com>
2021-06-16 07:59:59 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,44 @@
-console.log('%c HI', 'color: firebrick')
+
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  fetch(imgUrl)
+    .then(response => response.json())
+    .then(data => renderImages(data))
+
+  fetch(breedUrl)
+    .then(response => response.json)
+    .then(data => {
+      console.log(data)
+      console.log(typeof data)
+      debugger;
+    })
+
+})
+
+const renderImages = (json) => {
+  const imageHome = document.querySelector("#dog-image-container")
+  json.message.forEach(element => {
+    const img = document.createElement("img")
+    img.setAttribute("src", element)
+    imageHome.appendChild(img)
+  })
+}
+
+const renderAllBreeds = (json) => {
+  const dogBreeds = document.querySelector("#dog-breeds")
+  for (const breed in json.message) {
+    const li = document.createElement("li")
+    li.innerText = breed
+    dogBreeds.appendChild(li)
+
+  }
+}
+
+
+// fetch(breedUrl)
+// .then(response => response.json)
+// .then(data => console.log(data))
